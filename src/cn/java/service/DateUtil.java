31980a6adc98795670f95da5eee9cc3156763e6a
DT41guodong <13627265857@163.com>
2018-12-23 20:32:17 +0800

    GUODONG

--- src/cn/java/service/DateUtil.java
+++ src/cn/java/service/DateUtil.java
@@ -5,4 +5,8 @@ public class DateUtil {
 	public final static String node="1";
 	
 	public final static String FINAL_CURRECY_NODE="CNY";
+	
+	public final static String STATE_0="0";
+	
+	public final static String STATE_1="1";
 }
