b6d4c626f5cc8adecb16176a9e99d6602c1dce57
DT41guodong <13627265857@163.com>
2018-12-23 20:29:15 +0800

    1

---
+++ src/cn/java/service/DateUtil.java
@@ -0,0 +1,8 @@
+package cn.java.service;
+
+public class DateUtil {
+
+	public final static String node="1";
+	
+	public final static String FINAL_CURRECY_NODE="CNY";
+}
---
+++ src/cn/java/service/UserBase.java
@@ -0,0 +1,5 @@
+package cn.java.service;
+
+public class UserBase {
+
+}
