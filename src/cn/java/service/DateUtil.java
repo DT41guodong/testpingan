91f5d0c34a76a8f03aedaaeb8f3041f4493810d1
DT41guodong <32757150+DT41guodong@users.noreply.github.com>
2018-12-23 21:30:08 +0800

    Update DateUtil.java

--- src/cn/java/service/DateUtil.java
+++ src/cn/java/service/DateUtil.java
@@ -2,21 +2,11 @@ package cn.java.service;
 
 public class DateUtil {
 	
-	public final static String FINAL_CURRECY_NODE="CNY";
-	
 	public final static String STATE_0="0";
 	
 	public final static String STATE_1="1";
 	
 	public final static boolean flag3=false;
-	
-	public final static String FINAL_STATE="1";
 
-	public final static int a=2,b=1;
-	
-	public final static int c=2,d=1;
-	
-	public final static int e=1;
-	
-	public final static int f=1;
+	public final static String FINAL_CURRECY_NODE="CNY";
 }
