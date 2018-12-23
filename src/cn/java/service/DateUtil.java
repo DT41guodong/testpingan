f3c014eaa98cc9347c4660c0b2885432498dde74
DT41guodong <13627265857@163.com>
2018-12-23 21:54:44 +0800

    `1

--- src/cn/java/service/DateUtil.java
+++ src/cn/java/service/DateUtil.java
@@ -7,16 +7,10 @@ public class DateUtil {
 	public final static String STATE_0="0";
 	
 	public final static String STATE_1="1";
-	
-	public final static boolean flag3=false;
 
 	public final static int a=2,b=1;
 
 	public final static String FINAL_CURRECY_NODE1="CNY1";
-	
-	public final static String STATE_3="3";
-	
-	public final static String STATE_4="4";
 	
 	public final static String STATE_6="6";
 }
